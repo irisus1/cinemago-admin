7635cb800bb3912f6576ddc88bd4672bdace9b4d
Hoàng Tiến Đạt <datht@ad.aipower.vn>
2025-10-17 10:51:36 +0700

    export serive type

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+// Export all services
+export { default as authService } from './auth.service';
+
+// Export types
+export type { LoginRequest, User, LoginResponse, RegisterRequest } from './auth.service';
