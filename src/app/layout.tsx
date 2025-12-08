251513fae1a022f85d649f8e82d9d4516909fd68
irisus1 <22520210@gm.uit.edu.vn>
2025-12-08 11:11:33 +0700

    refactor: restructure admin layout and booking components for improved metadata handling and client-side rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,15 @@ import { AuthProvider } from "@/context/AuthContext";
 import { Toaster } from "@/components/ui/sonner";
 import "react-datepicker/dist/react-datepicker.css";
 import "./global.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s ", // %s sẽ được thay thế bởi tiêu đề của trang con
+    default: "CinemaGo - Admin", // Tiêu đề mặc định nếu trang con không set
+  },
+  description: "Hệ thống quản lý đặt vé xem phim",
+};
 
 export default function RootLayout({
   children,
